001c89274f36b92006207c0bbe9ad3e168064ceb
thomas0329 <thomas60808@gmail.com>
2025-11-28 14:20:37 +0800

    save reproduction

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 random_port=$(shuf -i 1000-9999 -n 1)
-for dataset in 'c12' 'c19' 'mimic_mortality' 'mimic_decompensation' 'mimic_phenotyping' 'mimic_lengthofstay'
+for dataset in  'c19'
 do
 for seed in 1 42 3407
 do
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nnodes 1 --nproc_per_node 4 --master_port $random_port \
-    main_pretrain.py --dataset $dataset --seed $seed
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nnodes 1 --nproc_per_node 4 --master_port $random_port \
-    main_finetune.py --dataset $dataset --seed $seed
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nnodes 1 --nproc_per_node 1 --master_port $random_port \
+    main_pretrain.py --dataset $dataset --seed $seed > pretrain_${dataset}_${seed}.log 2>&1
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nnodes 1 --nproc_per_node 1 --master_port $random_port \
+    main_finetune.py --dataset $dataset --seed $seed > finetune_${dataset}_${seed}.log 2>&1
 done
 done
