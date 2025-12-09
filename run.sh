02944d0f161358cbfe952144d9b483694cd50839
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2025-12-09 17:13:42 +0800

    best training config

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 random_port=$(shuf -i 1000-9999 -n 1)
-for dataset in 'c19'
+for dataset in c12 c19
 do
 for seed in 1 42 3407
 do
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nnodes 1 --nproc_per_node 1 --master_port $random_port \
-    main_pretrain.py --dataset $dataset --seed $seed
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nnodes 1 --nproc_per_node 1 --master_port $random_port \
-    main_finetune.py --dataset $dataset --seed $seed
+python main_pretrain.py --dataset $dataset --seed $seed --local-rank 0 --batch_size 256 --epochs 100
+python main_finetune.py --dataset $dataset --seed $seed --batch_size 256 --freeze_epochs 5
 done
 done
